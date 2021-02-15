7f2e8c1295578a1d9b792b01f6cd219d05e093c0
Raghav Kasrija <kasrijaraghav345@gmail.com>
2021-02-15 13:34:56 -0500

    deleted unused files

--- app/src/main/java/com/example/androidwithmars/DocumentsFragment.java
+++ app/src/main/java/com/example/androidwithmars/DocumentsFragment.java
@@ -5,7 +5,9 @@ import android.content.Intent;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
 
@@ -13,17 +15,10 @@ import android.widget.Button;
 public class DocumentsFragment extends Fragment {
 
 
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
-        Button button = null;
-        button.findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent i = new Intent(this, ActivityStore.class);
-                startActivity(i);
-            }
-        });
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_documents, container, false);
     }
 
 
